38737bd02a7be09bd29ce9c537fd8c284e32ab65
Andrea Pitto <andreabradpitto@gmail.com>
2020-07-16 16:54:01 +0200

    Destination folder for output
    Now it is easier and cleaner to handle results

--- smartwatch/src/offlineAnalysis.py
+++ smartwatch/src/offlineAnalysis.py
@@ -5,40 +5,48 @@ import pandas as pd
 
 
 font = {'family': 'serif',
-        'color':  'darkred',
+        'color': 'darkred',
         'weight': 'normal',
         'size': 12,
         }
+
 
 def plotData(df, subplt, x_axis, y_lable, titlePlot):
     plt.subplot(subplt)
     plt.plot(x_axis, df.X)
     plt.plot(x_axis, df.Y)
     plt.plot(x_axis, df.Z)
-    plt.xlabel('#samples', fontdict = font)
-    plt.ylabel(y_lable, fontdict = font)
-    plt.title(titlePlot, fontdict = font)
+    plt.xlabel('#samples', fontdict=font)
+    plt.ylabel(y_lable, fontdict=font)
+    plt.title(titlePlot, fontdict=font)
     plt.legend()
+
 
 if __name__ == '__main__':
 
-    df_linacc = pd.read_csv('lin_acc.csv',names=['X','Y','Z'], header = 0,decimal = ',') 
-    df_rot = pd.read_csv('orientation.csv',names=['X','Y','Z'],header = 0,decimal = ',')
-    df_angVel = pd.read_csv('angVel.csv',names=['X','Y','Z'],header = 0,decimal = ',')
-    
+    df_linacc = pd.read_csv('./src/smartwatch/src/output/lin_acc.csv',
+                            names=['X', 'Y', 'Z'], header=0, decimal=',')
+    df_rot = pd.read_csv('./src/smartwatch/src/output/orientation.csv', names=[
+                         'X', 'Y', 'Z'], header=0, decimal=',')
+    df_angVel = pd.read_csv('./src/smartwatch/src/output/angVel.csv', names=[
+                            'X', 'Y', 'Z'], header=0, decimal=',')
+
     # initialize x axis using number of rows of data
     t = len(df_linacc.X)
-    x_axis = np.arange(start = 1,stop = t + 1,step = 1)
+    x_axis = np.arange(start=1, stop=t + 1, step=1)
 
-    # plots data 
-    plt.figure(figsize = (12,12))
+    # plots data
+    plt.figure(figsize=(12, 12))
     temp = df_linacc.astype(float)
-    plotData(temp,311,x_axis,'Filtered linear acceleration [m/s^2]','Linear Acceleration')
+    plotData(temp, 311, x_axis,
+             'Filtered linear acceleration [m/s^2]', 'Linear Acceleration')
 
     temp = df_rot.astype(float)
-    plotData(temp,312,x_axis,'Filtered Orientation [deg]','Orientation angles (yaw, pitch, roll)')
+    plotData(temp, 312, x_axis,
+             'Filtered Orientation [deg]', 'Orientation angles (yaw, pitch, roll)')
 
     temp = df_angVel.astype(float)
-    plotData(temp,313,x_axis,'Filtered angular vel [rad/s]','Angular velocities')
+    plotData(temp, 313, x_axis,
+             'Filtered angular vel [rad/s]', 'Angular velocities')
 
     plt.show()
--- smartwatch/src/remove_gravity.py
+++ smartwatch/src/remove_gravity.py
@@ -7,123 +7,138 @@ from geometry_msgs.msg import Vector3
 import math
 import csv
 
-#flags used to turn on features
-flagWriteData = 1 # flagWriteData = 1 means to store simple data received from imu, after gravity removal
+# flags used to turn on features
+# flagWriteData = 1 means to store simple data received from imu, after gravity removal
+flagWriteData = 1
 
 #global variables
-g = [ 0, 0, 9.81]
-dx = 0.0174 # min angle perceived [rad], about 1 [deg]
-#dx = 0.087 # min angle perceived [rad], about 5 [deg]
-index = 1 #used to store data for offline analysis
+g = [0, 0, 9.81]
+dx = 0.0174  # min angle perceived [rad], about 1 [deg]
+# dx = 0.087 # min angle perceived [rad], about 5 [deg]
+index = 1  # used to store data for offline analysis
 
-#initialize files to store data
-with open('lin_acc.csv','w') as file:
-	writer = csv.writer(file)
-	writer.writerow(["","X","Y","Z"])
+# initialize files to store data
+with open('./src/smartwatch/src/output/lin_acc.csv', 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(["", "X", "Y", "Z"])
 
-with open('orientation.csv','w') as file:
-	writer = csv.writer(file)
-	writer.writerow(["","X","Y","Z"])
+with open('./src/smartwatch/src/output/orientation.csv', 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(["", "X", "Y", "Z"])
 
-with open('angVel.csv','w') as file:
-	writer = csv.writer(file)
-	writer.writerow(["","X","Y","Z"])
+with open('./src/smartwatch/src/output/angVel.csv', 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(["", "X", "Y", "Z"])
 
-def eulerAnglesToRotationMatrix(angles) : #angles [yaw, pitch, roll]
-    
-    R_z = np.array([[1,         0,                  0                     ],
-                    [0,         math.cos(angles[0]),   math.sin(angles[0]) ],
-                    [0,         -math.sin(angles[0]),  math.cos(angles[0]) ]
+
+def eulerAnglesToRotationMatrix(angles):  # angles [yaw, pitch, roll]
+
+    R_z = np.array([[1,         0,                  0],
+                    [0,         math.cos(angles[0]),   math.sin(angles[0])],
+                    [0,         -math.sin(angles[0]),  math.cos(angles[0])]
                     ])
-                    
-    R_y = np.array([[math.cos(angles[1]),    0,      -math.sin(angles[1])  ],
-                    [0,                      1,      0                    ],
-                    [math.sin(angles[1]),    0,      math.cos(angles[1])  ]
+
+    R_y = np.array([[math.cos(angles[1]),    0,      -math.sin(angles[1])],
+                    [0,                      1,      0],
+                    [math.sin(angles[1]),    0,      math.cos(angles[1])]
                     ])
-                
+
     R_x = np.array([[math.cos(angles[2]),      math.sin(angles[2]),     0],
                     [-math.sin(angles[2]),     math.cos(angles[2]),     0],
                     [0,                        0,                       1]
                     ])
-                                 
-    R = np.dot(R_z, np.dot( R_y, R_x ))
+
+    R = np.dot(R_z, np.dot(R_y, R_x))
 
     return R
 
-def anglesCompensate(angles) :
-	#reduce sensibility of sensor: minimum precision is dx
-	compensatedAngles = [0, 0, 0]
 
-	for i in range (0,3): # i = 0, 1, 2
-		if abs(angles[i]/ dx) >= 1 : 
-			compensatedAngles[i] = angles[i]
-		
-	return compensatedAngles
-	
+def anglesCompensate(angles):
+    # reduce sensibility of sensor: minimum precision is dx
+    compensatedAngles = [0, 0, 0]
+
+    for i in range(0, 3):  # i = 0, 1, 2
+        if abs(angles[i] / dx) >= 1:
+            compensatedAngles[i] = angles[i]
+
+    return compensatedAngles
+
+
 def removeGravity(lin_acc, Rot_m):
-	#rotate g vector in the current frame 
-	g_frame_i = np.dot(Rot_m, g)
-	g_removed = [0, 0, 0] # define linear acceleration without gravity
+    # rotate g vector in the current frame
+    g_frame_i = np.dot(Rot_m, g)
+    g_removed = [0, 0, 0]  # define linear acceleration without gravity
 
-	for i in range(0,3):
-		if lin_acc[i] >= 0:
-			g_removed[i] = lin_acc[i] - abs(g_frame_i[i])
-		if lin_acc[i] < 0:
-			g_removed[i] = lin_acc[i] + abs(g_frame_i[i])
+    for i in range(0, 3):
+        if lin_acc[i] >= 0:
+            g_removed[i] = lin_acc[i] - abs(g_frame_i[i])
+        if lin_acc[i] < 0:
+            g_removed[i] = lin_acc[i] + abs(g_frame_i[i])
 
-	return g_removed
+    return g_removed
+
 
 def storeDataInFiles(fileName, modality, data):
-	with open(fileName,modality) as file:
-		writer = csv.writer(file)
-		writer.writerow([ index,data[0], data[1], data[2] ])
+    with open(fileName, modality) as file:
+        writer = csv.writer(file)
+        writer.writerow([index, data[0], data[1], data[2]])
+
 
 def callback(data):
-	global index
+    global index
 
-	#get data
-	orientation = [data.orientation.x, data.orientation.y, data.orientation.z,data.orientation.w]
+    # get data
+    orientation = [data.orientation.x, data.orientation.y,
+                   data.orientation.z, data.orientation.w]
 
-	#transform quaternion to euler angles
-	angles = tf.transformations.euler_from_quaternion(orientation,"sxyz")
+    # transform quaternion to euler angles
+    angles = tf.transformations.euler_from_quaternion(orientation, "sxyz")
 
-	angular_velocity = [data.angular_velocity.x, data.angular_velocity.y, data.angular_velocity.z]
-	linear_acceleration = [data.linear_acceleration.x, data.linear_acceleration.y, data.linear_acceleration.z]
+    angular_velocity = [data.angular_velocity.x,
+                        data.angular_velocity.y, data.angular_velocity.z]
+    linear_acceleration = [data.linear_acceleration.x,
+                           data.linear_acceleration.y, data.linear_acceleration.z]
 
-	angles = anglesCompensate(angles)
-	
-	rot_matrix = eulerAnglesToRotationMatrix(angles)
+    angles = anglesCompensate(angles)
 
-	lin_acc_no_g = removeGravity(linear_acceleration, rot_matrix)
+    rot_matrix = eulerAnglesToRotationMatrix(angles)
 
-	if flagWriteData == 1:
-		#store data into .csv files in order to analyse them offline
-		storeDataInFiles('lin_acc.csv','a',lin_acc_no_g)
+    lin_acc_no_g = removeGravity(linear_acceleration, rot_matrix)
 
-		angles_in_deg = [(angles[0]*180) / math.pi, (angles[1]*180) / math.pi,(angles[2]*180) / math.pi]
-		storeDataInFiles('orientation.csv','a',angles_in_deg)
+    if flagWriteData == 1:
+        # store data into .csv files in order to analyse them offline
+        storeDataInFiles(
+            './src/smartwatch/src/output/lin_acc.csv', 'a', lin_acc_no_g)
 
-		storeDataInFiles('angVel.csv','a',angular_velocity)
+        angles_in_deg = [(angles[0]*180) / math.pi,
+                         (angles[1]*180) / math.pi, (angles[2]*180) / math.pi]
+        storeDataInFiles(
+            './src/smartwatch/src/output/orientation.csv', 'a', angles_in_deg)
 
-		index += 1
+        storeDataInFiles(
+            './src/smartwatch/src/output/angVel.csv', 'a', angular_velocity)
+
+        index += 1
+
 
 def listener():
 
-	# In ROS, nodes are uniquely named. If two nodes with the same
-	# name are launched, the previous one is kicked off. The
-	# anonymous = True flag means that rospy will choose a unique
-	# name for our 'listener' node so that multiple listeners can
-	# run simultaneously.
-	rospy.init_node('listener', anonymous = True)
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous = True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
 
-	# This declares that your node subscribes to the android/imu topic,
-	# which is of type sensor_msgs.msg.Imu. When new data is received,
-	# callback is invoked with that data as argument. 
-	rospy.Subscriber("android/imu", Imu, callback)
+    # This declares that your node subscribes to the android/imu topic,
+    # which is of type sensor_msgs.msg.Imu. When new data is received,
+    # callback is invoked with that data as argument.
+    rospy.Subscriber("android/imu", Imu, callback)
 
-	# spin() simply keeps python from exiting until this node is stopped
-	rospy.spin()
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
 
 if __name__ == '__main__':
-	
-	listener()
+
+    listener()
