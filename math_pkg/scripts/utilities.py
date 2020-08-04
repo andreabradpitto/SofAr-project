9fbb6a4cc1487534ee5788815109f9922c6f730b
Geraldone <49336509+Geraldone@users.noreply.github.com>
2020-08-05 00:58:54 +0200

    added some methods needed in forward

--- math_pkg/scripts/utilities.py
+++ math_pkg/scripts/utilities.py
@@ -1,3 +1,4 @@
+import numpy as np
 from std_msgs.msg import Float64MultiArray, MultiArrayDimension
 
 def init_float64_multiarray(rows,columns):
@@ -15,3 +16,46 @@ def init_float64_multiarray(rows,columns):
     a.layout.dim[1].label ="columns"
     a.layout.dim[1].size = columns
     return a
+
+def anglesCompensate(angles):
+    """!
+    Function used to filter unwanted minimal incoming data fluctuations,
+    due to noise as well as human operator shake
+    @param orientation with respect to X, Y, Z axes
+    @returns returns a filtered version (if necessary) of the input angles
+    """
+    dx = 0.0174  # min angle perceived [rad], about 1 [deg]
+    # reduce sensibility of sensor: minimum precision is dx
+    compensatedAngles = [0, 0, 0]
+
+    for i in range(0, 3):  # i = 0, 1, 2
+        if abs(angles[i] / dx) >= 1:
+            compensatedAngles[i] = angles[i]
+
+    return compensatedAngles
+
+def eulerAnglesToRotationMatrix(angles):  # angles [roll, pitch, yaw]
+    """!
+    Function that transforms euler angle coordinates into the rotation matrix
+    @param angles euler angles, i.e. orientation with respect to X, Y, Z axes
+    @returns rotation matrix
+    """
+
+    R_x = np.array([[1,         0,                  0],
+                    [0,         np.cos(angles[0]),   np.sin(angles[0])],
+                    [0,         -np.sin(angles[0]),  np.cos(angles[0])]
+                    ])
+
+    R_y = np.array([[np.cos(angles[1]),    0,      -np.sin(angles[1])],
+                    [0,                      1,      0],
+                    [np.sin(angles[1]),    0,      np.cos(angles[1])]
+                    ])
+
+    R_z = np.array([[np.cos(angles[2]),      np.sin(angles[2]),     0],
+                    [-np.sin(angles[2]),     np.cos(angles[2]),     0],
+                    [0,                        0,                       1]
+                    ])
+
+    R = np.dot(R_x, np.dot(R_y, R_z))
+
+    return R
