9ca56e0d88f0ae21d66635b088f4378ca5afc166
LucaTars <42372876+LucaTars@users.noreply.github.com>
2020-08-08 11:36:13 +0200

    updated utilities.h for test

--- math_pkg/src/utilities.h
+++ math_pkg/src/utilities.h
@@ -27,12 +27,14 @@ using namespace sensor_msgs;
 #define JOINTS_MARGIN 0.2
 /*! Max absolute value of correction pole for joint angles.*/
 #define JOINTS_MAXPOLE 3
-/*! Linear positional gain for tracking.*/
-#define Kp 16
+/*! Linear positional gain for tracking in CLIK1.*/
+#define Kpp 25
+/*! Linear positional gain for tracking in CLIK2.*/
+#define Kp 400
 /*! Linear velocity gain for tracking.*/
-#define Kv 8
+#define Kv 40
 /*! Rotational gain for tracking.*/
-#define Krot 2
+#define Krot 10
 /*! Number of robot joint.*/
 #define NJOINTS 7
 /*! Number of invkin services.*/
@@ -51,6 +53,8 @@ using namespace sensor_msgs;
 #define VEL_MARGIN 0.2
 /*! Max absolute value of correction pole for joint velocity.*/
 #define VEL_MAXPOLE 20
+/*! Abs value of the correction pole for safety task.*/
+#define CORRPOLE 70
 
 /*! Min joint angles.*/
 const double QMIN[] = {-2.4409, -0.5560, -2.9994, -0.0324, -3.0343, -1.5508, -3.0343};
@@ -62,8 +66,6 @@ const double QDOTMIN[] = {-3,-3,-3,-3,-3,-3,-3};
 const double QDOTMAX[] = {3,3,3,3,3,3,3};
 /*! Initial joint angles.*/
 const double QINIT[] = {0,0,0,0,0,0,0};
-/*! Weights for invkin solutions (sum must be 1).*/
-const double WEIGHTS[] = {0.1,0.1,0,0.8,0,0};
 /*! Constant used in Gaussian computation for pseudoinversion.*/
 const double b = -log(0.5)/0.01;
 /*! Identity matrix of size NJOINTS.*/
@@ -94,8 +96,7 @@ inline double cos_sigmoid(double x,double y,double mrgn) {
     \param xmin Min value of x.
     \param xmax Max value of x.
     \param mrgn Soft margin of x.
-    \param maxPole Max absolute value of the correction pole for x.
-    \param maj A majorant of x.
+    \param corrPole Absolute value of correction pole.
     \param currentPole Current correction pole for x, if any, 0 otherwise.
     \param isJoint True if x is a joint angle, false otherwise.
     \param rdot Reference to task element derivative for quantity x.
@@ -103,12 +104,12 @@ inline double cos_sigmoid(double x,double y,double mrgn) {
     \return true if no correction needed, false otherwise.
 */
 bool jointConstr(double x,const double xmin,const double xmax,
-	const double mrgn,const double maxPole,const double maj,
+	const double mrgn,const double corrPole,
 	double &currentPole,const bool isJoint,double &rdot, double &Adiag) {
     bool ok = false;
 	if (x > xmax - mrgn) {
         //cout << "too high" << x << ">" << xmax - mrgn << ", is joint = " << isJoint << endl;
-		if (currentPole == 0) currentPole = min(maxPole,float(maj)/abs(x-xmax));
+		if (currentPole == 0) currentPole = corrPole;
 		if (isJoint) rdot = currentPole * (-x + xmax - mrgn);
 		else rdot = currentPole * (-x + xmax - mrgn) * DT + x;
 		if (x > xmax) Adiag = 1;
@@ -116,7 +117,7 @@ bool jointConstr(double x,const double xmin,const double xmax,
 	}
 	else if (x < xmin + mrgn) {
         //cout << "too low" << x << "<" << xmin + mrgn << ", is joint = " << isJoint << endl;
-		if (currentPole == 0) currentPole = min(maxPole,float(maj)/abs(x-xmin));
+		if (currentPole == 0) currentPole = corrPole;
 		if (isJoint) rdot = currentPole * (-x + xmin + mrgn);
 		else rdot = currentPole * (-x + xmin + mrgn) * DT + x;
 		if (x < xmin) Adiag = 1;
@@ -191,7 +192,7 @@ MatrixXd regPinv(MatrixXd X,MatrixXd A,MatrixXd Q,double eta) {
 void computeqdot(VectorXd partialqdot,MatrixXd Q1,MatrixXd J,MatrixXd JL,
     MatrixXd JLdot,VectorXd qdot,VectorXd eta,VectorXd rho,VectorXd etadot,
     VectorXd v,VectorXd w,VectorXd a,VectorXd &qdot1,VectorXd &qdot2) {
-    VectorXd ve1 = v + Kp*eta;
+    VectorXd ve1 = v + Kpp*eta;
     VectorXd ve2 = DT*(a - JLdot*qdot + Kv*etadot + Kp*eta) + JL*qdot;
     VectorXd xedot1 = VectorXd(6);
     VectorXd xedot2 = VectorXd(6);
