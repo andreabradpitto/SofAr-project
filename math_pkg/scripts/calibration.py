5b7cf58194a5211b45609cf3b61923df0ac2e1d4
Geraldone <49336509+Geraldone@users.noreply.github.com>
2020-08-29 21:26:01 +0200

    Update calibration.py

--- math_pkg/scripts/calibration.py
+++ math_pkg/scripts/calibration.py
@@ -240,7 +240,7 @@ def calibrate():
     rospy.init_node('calibrate', anonymous=True)
 
     rospy.subscriber("smartphone", Imu, imu_ee_calibration)
-    rospy.Subscriber("handleSimulation", Int8, simulate_callback)
+    rospy.subscriber("handleSimulation", Int8, simulate_callback)
 
     rospy.spin()
 
