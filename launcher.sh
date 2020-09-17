77080eb12537b11101b4a27eb304aa98d00d63fe
Matteo <matteo.palmas7@gmail.com>
2020-09-17 11:40:56 +0200

    minor corrections

--- launcher.sh
+++ launcher.sh
@@ -21,9 +21,9 @@ gnome-terminal -x sh -c "rosrun smartphone clipping.py; bash"
 gnome-terminal -x sh -c "roslaunch --wait math_pkg mathAll.launch; bash"
 
 #launch coppelia enviroment
-#roslaunch V-rep baxter_sim.launch
+roslaunch V-rep baxter_sim.launch
 
 kill $(ps aux | grep "sh -c rosrun" | tr -s ' '| cut -d ' ' -f 2)
 kill $(ps aux | grep "sh -c roslaunch" | tr -s ' '| cut -d ' ' -f 2)
-
+killall -9 roscore
 
